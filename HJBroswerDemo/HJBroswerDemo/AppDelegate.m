b9d1d02732b6ff44e9fbb83374171253c8d1742d
zuweizhong <2416086440@qq.com>
2016-12-29 13:09:25 +0800

    no message

--- HJBroswerDemo/HJBroswerDemo/AppDelegate.m
+++ HJBroswerDemo/HJBroswerDemo/AppDelegate.m
@@ -19,6 +19,7 @@
     // Override point for customization after application launch.
     
     self.window = [[UIWindow alloc]init];
+    self.window.frame = [UIScreen mainScreen].bounds;
     self.window.backgroundColor = [UIColor whiteColor];
     
     self.window.rootViewController = [ViewController demoCollectionViewController];
